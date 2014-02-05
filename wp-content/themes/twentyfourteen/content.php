7efdde467af34710a1e8726aaf5d437fc44ccaa8
nedapp <nedeljkoo@yahoo.com>
2014-02-05 19:30:11 +0100

    Add blog page. Add single page.

--- wp-content/themes/twentyfourteen/content.php
+++ wp-content/themes/twentyfourteen/content.php
@@ -51,7 +51,7 @@
 	<?php else : ?>
 	<div class="entry-content">
 		<?php
-			the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
+			the_excerpt();
 			wp_link_pages( array(
 				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
 				'after'       => '</div>',
--- wp-content/themes/twentyfourteen/functions.php
+++ wp-content/themes/twentyfourteen/functions.php
@@ -534,3 +534,10 @@ add_action( 'wp_enqueue_script', 'load_jquery' );
 function load_jquery() {
     wp_enqueue_script( 'jquery' );
 }
+
+// Replaces the excerpt "more" text by a link
+function new_excerpt_more($more) {
+ 	global $post;
+	return '   <a class="moretag" href="'. get_permalink($post->ID) . '"> >> </a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
